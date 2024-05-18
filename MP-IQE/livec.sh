e09d1659045014a092cc0603825d8360e722622d
潘文胜 <2635551571@qq.com>
2024-05-18 18:26:00 +0800

    更新

--- MP-IQE/livec.sh
+++ MP-IQE/livec.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --nproc_per_node=1 --master_port=25675 train2.py \
+CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --nproc_per_node=1 --master_port=25675 train.py \
     --cfg configs/Pure/vit_small_pre_coder_livec.yaml  \
     --tensorboard --tag livec \
     --dist --scene \
